4d7beefba511eda72f600f093136cb65e782b138
Jack Kong <jfn.kong@gmail.com>
2019-12-03 00:29:02 -0600

    'Added recursion for non valid inputs and flags'

--- Simple_Python_Calculator.py
+++ Simple_Python_Calculator.py
@@ -28,7 +28,12 @@ def root(x, y):
     """Return the n root of an integer argument by a factor of a second integer argument."""
     return x**(1/float(y))
 
-dict_operations = {'options':['add', 'sub', 'multi', 'divide', 'exp', 'root']}
+dict_operations = {'options':{'add':' perform the addition of two integers',
+                              'sub':' perform the subtraction of two integers', 
+                              'multi':' perform the multiplication of two integers', 
+                              'divide':' perform the division of two numbers', 
+                              'exp':' get the result of a base integer with exponential factor of another integer', 
+                              'root':' get the nth integer root of a base integer'}}
 dict_functions = {
     'add': add,
     'sub': sub,
@@ -38,26 +43,31 @@ dict_functions = {
     'root' : root,
 }
 
+active = True #Flag set for program runtime
+
 print("Welcome to the Simple Python Calculator. Please enter an operation to perform.")
 
-while True:
+while active:
     sel = input("\nEnter operation on this line or enter help to see options or quit: ")
 
     if sel == 'help':
         for key, vals in dict_operations.items():
             print("Here are your choices of "+ key +":")
-            for each in vals:
-                print("\n" + each)
+            for each, description in vals.items():
+                print("\n" + each + ':' + description + '.')
 
     elif sel == 'quit':
-        break
-    
-    else:
-        arg1 = int(input("\nEnter the first integer argument of your operation: "))
-        arg2 = int(input("\nEnter the second integer argument of your operation: "))
-        for defs in dict_operations['options']:
-            if sel == defs:
-                print("\nThe result is: " + str(dict_functions[sel](arg1,arg2)))
+        active = False 
 
+    else:
+        for each in dict_functions.keys():
+            if sel == each :
+                arg1 = int(input("\nEnter the first integer argument of your operation: "))
+                arg2 = int(input("\nEnter the second integer argument of your operation: "))
+                for defs in dict_operations['options']:
+                    if sel == defs:
+                        print("\nThe result is: " + str(dict_functions[sel](arg1,arg2)))
+            else:
+                continue          
 
 print("\nThank you for using the Simple Python Calculator.")
